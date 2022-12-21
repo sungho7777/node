e78c586cb814b1da2fb4014b90bf53d08ed94333
mzncvmc <mzncvmc@gmail.com>
2022-12-21 23:01:40 +0900

    20221221 npm install figlet

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello world~!');
+var figlet = require('figlet');
+
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
