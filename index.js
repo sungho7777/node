b32d1ef014fc331c288bade0f96186b0af9b3a83
mzncvmc <mzncvmc@gmail.com>
2022-12-22 22:07:07 +0900

    20221222 API server...

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-var figlet = require('figlet');
+// https://expressjs.com/ko/starter/hello-world.html
 
-figlet('Hello World!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data);
-});
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 8080
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/dog', (req, res) => {
+  res.json({'sound':'Hello dog!'})
+})
+app.get('/cat', (req, res) => {
+  res.json({'sound':'Hello cat!'})
+})
+app.get('/user/:id', (req, res) => {
+  const q = req.params;
+  res.send('Hello ' + q.id);
+})
+app.get('/sound/:name', (req, res) => {
+  const { name } = req.params;
+  console.log(name);
+  res.json({'sound':name})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+// https://www.youtube.com/watch?v=Tt_tKhhhJqY
+// 19분 19초
